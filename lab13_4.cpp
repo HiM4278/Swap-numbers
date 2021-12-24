9ae719fbc019171bb5517ad65ba7b4b3bb0c73ef
HiM <pasakorn141@gmail.com>
2021-12-24 14:44:26 +0700

    lab13_4 completed

--- lab13_4.cpp
+++ lab13_4.cpp
@@ -2,6 +2,9 @@
 
 using namespace std;
 
+template<typename input>
+void mySwap(input & ,input & );
+
 int main(){
 	int x, y;
 	string a, b;
@@ -28,5 +31,12 @@ int main(){
 	cout << "p = " << p << ", q = " << q << "\n";
 
 	return 0;
+}
+
+template<typename input>
+void mySwap(input &x ,input &y){
+	input z = x,m = y;
+	x = m;
+	y = z;
 }
 
